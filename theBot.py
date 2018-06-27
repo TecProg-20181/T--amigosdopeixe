2215138ff30b0cd5a0e03a9169b6abd07e7896ff
Matheus Gomes <matheusgomesf01@gmail.com>
2018-06-26 23:30:27 -0300

    new class handletask

--- theBot.py
+++ theBot.py
@@ -2,13 +2,14 @@ import json
 import requests
 import urllib
 from person import Person
+from emojis import Emojis
 
 class PeixeBot():
     def __init__(self):
         self.tokenread = Person.showToken("token.txt")
         self.loginread = Person.showLogin("login.txt")
         self.passwordread = Person.showPassword("password.txt")
-        self.URL = "https://api.telegram.org/bot{}/".format(tokenread.rstrip())
+        self.URL = "https://api.telegram.org/bot{}/".format(self.tokenread.rstrip())
 
         self.HELP = """
                     /new NOME
@@ -22,48 +23,48 @@ class PeixeBot():
                     /duplicate ID
                     /duedate ID DATE(mm/dd/YYYY)
                     /priority ID PRIORITY{low, medium, high}
-                    priority low = """ + Emojis.EMOJI_LOW.value + """
-                    priority medium = """ + Emojis.EMOJI_MEDIUM.value + """
-                    priority high = """ + Emojis.EMOJI_HIGH.value + """
+                    priority low = """ + Emojis.EMOJI_LOW + """
+                    priority medium = """ + Emojis.EMOJI_MEDIUM + """
+                    priority high = """ + Emojis.EMOJI_HIGH + """
                     /help
                     """
 
-    def get_url(url):
+    def get_url(self, url):
         response = requests.get(url)
         content = response.content.decode("utf8")
         return content
 
-    def get_json_from_url(url):
-        content = get_url(url)
+    def get_json_from_url(self, url):
+        content = self.get_url(url)
         js = json.loads(content)
         return js
 
-    def get_updates(offset=None):
-        url = URL + "getUpdates?timeout=100"
+    def get_updates(self, offset=None):
+        url = self.URL + "getUpdates?timeout=100"
         if offset:
             url += "&offset={}".format(offset)
-        js = get_json_from_url(url)
+        js = self.get_json_from_url(url)
         return js
 
-    def send_message(text, chat_id, reply_markup=None):
+    def send_message(self, text, chat_id, reply_markup=None):
         text = urllib.parse.quote_plus(text)
-        url = URL + "sendMessage?text={}&chat_id={}\
+        url = self.URL + "sendMessage?text={}&chat_id={}\
                     &parse_mode=Markdown".format(text, chat_id)
         if reply_markup:
             url += "&reply_markup={}".format(reply_markup)
-        get_url(url)
+        self.get_url(url)
     
-    def get_last_update_id(updates):
+    def get_last_update_id(self, updates):
         update_ids = []
         for update in updates["result"]:
             update_ids.append(int(update["update_id"]))
 
         return max(update_ids)
     
-    def create_issue(title, body=None):
+    def create_issue(self, title, body=None):
         url = 'https://api.github.com/repos/TecProg-20181/T--amigosdopeixe/issues'
         request = requests.Session()
-        request.auth =(loginread.rstrip(), passwordread.rstrip())
+        request.auth =(self.loginread.rstrip(), self.passwordread.rstrip())
         issue = {'title': title,
                 'body': body}
         post = request.post(url, json.dumps(issue))
